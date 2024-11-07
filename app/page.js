f5b1942266dfd733ea430171c44c7e092dd50117
Bright <brightagu2@gmail.com>
2024-11-07 12:18:51 +0100

    update

--- app/page.js
+++ app/page.js
@@ -416,6 +416,299 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className="price-area section-gap">
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            <div className="menu-content pb-70 col-lg-8">
+              <div className="title text-center">
+                <h1 className="mb-10">We Provide Affordable Prices</h1>
+                <p>
+                  Well educated, intellectual people, especially scientists at
+                  all times demonstrate considerably.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="single-price">
+                <h4>Cheap Packages</h4>
+                <ul className="price-list">
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>New York</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Maldives</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Sri Lanka</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Nepal</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Thiland</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Singapore</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="single-price">
+                <h4>Luxury Packages</h4>
+                <ul className="price-list">
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>New York</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Maldives</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Sri Lanka</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Nepal</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Thiland</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Singapore</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="single-price">
+                <h4>Camping Packages</h4>
+                <ul className="price-list">
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>New York</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Maldives</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Sri Lanka</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Nepal</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Thiland</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                  <li className="d-flex justify-content-between align-items-center">
+                    <span>Singapore</span>
+                    <a href="#" className="price-btn">
+                      $1500
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer-area section-gap">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3 col-md-6 col-sm-6">
+              <div className="single-footer-widget">
+                <h6>About Agency</h6>
+                <p>
+                  The world has become so fast paced that people don’t want to
+                  stand by reading a page of information, they would much rather
+                  look at a presentation and understand the message. It has come
+                  to a point
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6">
+              <div className="single-footer-widget">
+                <h6>Navigation Links</h6>
+                <div className="row">
+                  <div className="col">
+                    <ul>
+                      <li>
+                        <a href="#">Home</a>
+                      </li>
+                      <li>
+                        <a href="#">Feature</a>
+                      </li>
+                      <li>
+                        <a href="#">Services</a>
+                      </li>
+                      <li>
+                        <a href="#">Portfolio</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="col">
+                    <ul>
+                      <li>
+                        <a href="#">Team</a>
+                      </li>
+                      <li>
+                        <a href="#">Pricing</a>
+                      </li>
+                      <li>
+                        <a href="#">Blog</a>
+                      </li>
+                      <li>
+                        <a href="#">Contact</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6">
+              <div className="single-footer-widget">
+                <h6>Newsletter</h6>
+                <p>
+                  For business professionals caught between high OEM price and
+                  mediocre print and graphic output.
+                </p>
+                <div id="mc_embed_signup">
+                  <form
+                    target="_blank"
+                    action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                    method="get"
+                    className="subscription relative"
+                  >
+                    <div className="input-group d-flex flex-row">
+                      <input
+                        name="EMAIL"
+                        placeholder="Email Address"
+                        onfocus="this.placeholder = ''"
+                        onblur="this.placeholder = 'Email Address '"
+                        required=""
+                        type="email"
+                      />
+                      <button className="btn bb-btn">
+                        <span className="lnr lnr-location"></span>
+                      </button>
+                    </div>
+                    <div className="mt-10 info"></div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6">
+              <div className="single-footer-widget mail-chimp">
+                <h6 className="mb-20">InstaFeed</h6>
+                <ul className="instafeed d-flex flex-wrap">
+                  <li>
+                    <img src="img/i1.jpg" alt="" />
+                  </li>
+                  <li>
+                    <img src="img/i2.jpg" alt="" />
+                  </li>
+                  <li>
+                    <img src="img/i3.jpg" alt="" />
+                  </li>
+                  <li>
+                    <img src="img/i4.jpg" alt="" />
+                  </li>
+                  <li>
+                    <img src="img/i5.jpg" alt="" />
+                  </li>
+                  <li>
+                    <img src="img/i6.jpg" alt="" />
+                  </li>
+                  <li>
+                    <img src="img/i7.jpg" alt="" />
+                  </li>
+                  <li>
+                    <img src="img/i8.jpg" alt="" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="row footer-bottom d-flex justify-content-between align-items-center">
+            <p className="col-lg-8 col-sm-12 footer-text m-0">
+              Copyright &copy;
+              <script>document.write(new Date().getFullYear());</script>
+              All rights reserved | This template is made with
+              <i className="fa fa-heart-o" aria-hidden="true"></i> by
+              <a href="https://colorlib.com" target="_blank">
+                Colorlib
+              </a>
+            </p>
+            <div className="col-lg-4 col-sm-12 footer-social">
+              <a href="#">
+                <i className="fa fa-facebook"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-dribbble"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-behance"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
